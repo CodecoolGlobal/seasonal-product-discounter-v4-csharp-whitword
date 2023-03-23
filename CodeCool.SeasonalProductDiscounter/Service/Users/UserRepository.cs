021cb48310a8dd9ef3ea759d6b8dbab5d7ff69d4
whitword <gyorgyi.lajterne@gmail.com>
2023-03-23 17:33:51 +0100

    Complete UserRepository

--- CodeCool.SeasonalProductDiscounter/Service/Users/UserRepository.cs
+++ CodeCool.SeasonalProductDiscounter/Service/Users/UserRepository.cs
@@ -3,6 +3,7 @@ using System.Runtime.InteropServices;
 using CodeCool.SeasonalProductDiscounter.Model.Users;
 using CodeCool.SeasonalProductDiscounter.Service.Logger;
 using CodeCool.SeasonalProductDiscounter.Service.Persistence;
+using Microsoft.Data.Sqlite;
 
 namespace CodeCool.SeasonalProductDiscounter.Service.Users;
 
@@ -40,18 +41,38 @@ public class UserRepository : SqLiteConnector, IUserRepository
 
     public bool Add(User user)
     {
-        //
+        var query = @$"INSERT INTO {DatabaseManager.UsersTableName} (`user_name`, `password`) VALUES(@userName, @password)";
+        try
+        {
+            using var connection = GetPhysicalDbConnection();
+            using var command = GetCommand(query, connection);
+            using var reader = command.ExecuteReader();
+            SqliteCommand myCommand = new SqliteCommand(query, connection);
+            myCommand.Parameters.AddWithValue("@userName", user.UserName);
+            myCommand.Parameters.AddWithValue("@password", user.Password);
+            myCommand.ExecuteNonQuery();
+            //connection.Close();
+
+        }
+        catch (Exception e)
+        {
+            Logger.LogError(e.Message);
+            throw;
+        }
         return false;
     }
 
     public User Get(string name)
     {
-        var query = "";
-
+        var query = @$"SELECT * FROM users WHERE user_name LIKE %{name}%";
+        
         try
         {
-            //
-            return null;
+            using var connection = GetPhysicalDbConnection();
+            using var command = GetCommand(query, connection);
+            using var reader = command.ExecuteReader();
+            var user = new User(reader.GetInt32(0), reader.GetString(1), reader.GetString(2));
+            return user;
         }
         catch (Exception e)
         {
