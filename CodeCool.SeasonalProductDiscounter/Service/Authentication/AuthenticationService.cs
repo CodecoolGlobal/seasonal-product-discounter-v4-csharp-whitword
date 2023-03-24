d5fe6faa0cd682cc5078d0cb376bcc10169ae6a2
whitword <gyorgyi.lajterne@gmail.com>
2023-03-24 22:11:19 +0100

    Complete AuthenticationService

--- CodeCool.SeasonalProductDiscounter/Service/Authentication/AuthenticationService.cs
+++ CodeCool.SeasonalProductDiscounter/Service/Authentication/AuthenticationService.cs
@@ -15,6 +15,11 @@ public class AuthenticationService : IAuthenticationService
 
     public bool Authenticate(User user)
     {
+        var users = _userRepository.GetAll();
+        foreach (var listUser in users)
+        {
+            if (listUser.UserName == user.UserName && listUser.Password == user.Password) return true;
+        }
         return false;
     }
 }
